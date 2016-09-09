fb7df2363b6348d443182df61a41bef034a13643
Sean Larkin <sean.larkin@cuw.edu>
2016-09-08 22:13:09 -0500

    initial commit

---
+++ V8LazyParsedFunctionModulePlugin.js
@@ -0,0 +1,18 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+var V8LazyParsedFunctionModuleTemplatePlugin = require("./V8LazyParsedFunctionModuleTemplatePlugin");
+var RequestShortener = require("webpack/lib/RequestShortener");
+
+function V8LazyParsedFunctionModulePlugin(options, requestShortener) {
+	this.options = options;
+	this.requestShortener = requestShortener;
+}
+module.exports = V8LazyParsedFunctionModulePlugin;
+V8LazyParsedFunctionModulePlugin.prototype.apply = function(compiler) {
+	compiler.plugin("this-compilation", function(compilation) {
+		compilation.moduleTemplate.requestShortener = this.requestShortener || new RequestShortener(compiler.context);
+		compilation.moduleTemplate.apply(new V8LazyParsedFunctionModuleTemplatePlugin());
+	}.bind(this));
+};
